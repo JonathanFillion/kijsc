a5bb636658b760a947a35dcf9e3ca84bec7f96cd
deliverapi <deliverapi@gmail.com>
2020-08-20 11:50:31 -0400

    push for deploy

--- index.js
+++ index.js
@@ -1,19 +1,185 @@
+
+require('console-stamp')(console, { pattern: 'dd/mm/yyyy HH:MM:ss.l' });
 const htmlparser2 = require("htmlparser2");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
+const notifier = require('node-notifier');
+const pause = ms => new Promise(res => setTimeout(res, ms))
 
-let url = "https://www.kijiji.ca/b-video-games-consoles/gatineau/gamecube/k0c141l1700186"
-let previousAds = []
+let urlGamecube = "https://www.kijiji.ca/b-ottawa-gatineau-area/gamecube/k0l1700184?dc=true"
+let urlSwitch = "https://www.kijiji.ca/b-video-games-consoles/gatineau/switch/k0c141l1700186?ll=45.394093%2C-75.851320&address=29+Rue+du+Patrimoine%2C+Gatineau%2C+QC+J9H+3N6%2C+Canada&radius=60.0"
+let urlPs2 = "https://www.kijiji.ca/b-video-games-consoles/gatineau/ps2/k0c141l1700186?ll=45.394093%2C-75.851320&address=29+Rue+du+Patrimoine%2C+Gatineau%2C+QC+J9H+3N6%2C+Canada&radius=60.0"
+let urlPs1 = "https://www.kijiji.ca/b-video-games-consoles/gatineau/ps1/k0c141l1700186?ll=45.394093%2C-75.851320&address=29+Rue+du+Patrimoine%2C+Gatineau%2C+QC+J9H+3N6%2C+Canada&radius=60.0"
+let urlDs = "https://www.kijiji.ca/b-video-games-consoles/gatineau/nintendo-ds/k0c141l1700186?ll=45.394093%2C-75.851320&address=29+Rue+du+Patrimoine%2C+Gatineau%2C+QC+J9H+3N6%2C+Canada&radius=60.0"
+
+
+
+let previousAdsGamecube = []
+let previousAdsSwitch = []
+let previousAdsPs2 = []
+let previousAdsPs1 = []
+let previousAdsDs = []
+
+
 let delay
 let intervalId
 
 let getKijiji = async () => {
-	console.log("getKijiji")
-	fetch(url)
+	console.log("getKijiji()")
+
+
+	/*Gamecube*/
+	fetch(urlGamecube)
 	.then(res => res.text())
 	.then(function(body) {
 		let ads = getAllAds(body)
-		previousAds = ads
+		console.log(ads[0])
+		if(previousAdsGamecube.length){
+			if(previousAdsGamecube[0].title !== ads[0].title){
+
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				console.log("New add detected !")
+				console.log("Add Title: " + ads[0].title)
+				console.log("URL : " + url)
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				
+				notifier.notify({
+					title:"Une annonce a changé dans la recherche 'gamecube' !",
+					message: ads[0].title,
+					sound: true,
+				})
+			}
+		}
+		previousAdsGamecube = ads
+	})
+
+	pause(2000)
+
+
+	/*Switch*/
+	fetch(urlSwitch)
+	.then(res => res.text())
+	.then(function(body) {
+		let ads = getAllAds(body)
+		console.log(ads[0])
+		if(previousAdsSwitch.length){
+			if(previousAdsSwitch[0].title !== ads[0].title){
+
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				console.log("New add detected !")
+				console.log("Add Title: " + ads[0].title)
+				console.log("URL : " + url)
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				
+				notifier.notify({
+					title:"Une annonce a changé dans la recherche 'switch' !",
+					message: ads[0].title,
+					sound: true,
+				})
+			}
+		}
+		previousAdsSwitch = ads
+	})
+
+	pause(2000)
+
+	/*PS2*/
+	fetch(urlPs2)
+	.then(res => res.text())
+	.then(function(body) {
+		let ads = getAllAds(body)
+		console.log(ads[0])
+		if(previousAdsPs2.length){
+			if(previousAdsPs2[0].title !== ads[0].title){
+
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				console.log("New add detected !")
+				console.log("Add Title: " + ads[0].title)
+				console.log("URL : " + url)
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				
+				notifier.notify({
+					title:"Une annonce a changé dans la recherche 'ps2' !",
+					message: ads[0].title,
+					sound: true,
+				})
+			}
+		}
+		previousAdsPs2 = ads
+	})
+
+	pause(2000)
+
+	/*PS1*/
+	fetch(urlPs1)
+	.then(res => res.text())
+	.then(function(body) {
+		let ads = getAllAds(body)
+		console.log(ads[0])
+		if(previousAdsPs1.length){
+			if(previousAdsPs1[0].title !== ads[0].title){
+
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				console.log("New add detected !")
+				console.log("Add Title: " + ads[0].title)
+				console.log("URL : " + url)
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				
+				notifier.notify({
+					title:"Une annonce a changé dans la recherche 'ps1' !",
+					message: ads[0].title,
+					sound: true,
+				})
+			}
+		}
+		previousAdsPs1 = ads
+	})
+
+	pause(2000)
+
+	/*PS1*/
+	fetch(urlDs)
+	.then(res => res.text())
+	.then(function(body) {
+		let ads = getAllAds(body)
+		console.log(ads[0])
+		if(previousAdsDs.length){
+			if(previousAdsDs[0].title !== ads[0].title){
+
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				console.log("New add detected !")
+				console.log("Add Title: " + ads[0].title)
+				console.log("URL : " + url)
+				console.log()
+				console.log("--------------------------------------")
+				console.log()
+				
+				notifier.notify({
+					title:"Une annonce a changé dans la recherche 'ds' !",
+					message: ads[0].title,
+					sound: true,
+				})
+			}
+		}
+		previousAdsDs = ads
 	})
 }
 
@@ -27,9 +193,17 @@ let getAllAds = (html) => {
 		let price = cleanPrice($('.price', element).text())
 		let description = cleanDescription($('.description', element).text())
 		let time = $('.date-posted', element).text()
-		tempAds.push({title:title, href:href, price:price, description:description,time:time})
+		let ad = {title:title, href:href, price:price, description:description,time:time}
+		if(adIsValid(ad)){
+			tempAds.push(ad)
+		}
 	})
 	return tempAds
+}
+
+let adIsValid = (ad) => {
+	valid = !ad.title.includes("wanted:")
+	return valid
 }
 
 let cleanTitle = (text) => {
@@ -38,7 +212,7 @@ let cleanTitle = (text) => {
 	text = text.trim()
 	text = text.replace("\n", "")
 	text = text.replace("                            ", "")
-	return text
+	return text.toLowerCase()
 }
 
 let cleanPrice = (text) => {
@@ -52,12 +226,16 @@ let cleanDescription = (text) => {
 	text = text.replace("\n", "")
 	text = text.replace("\r", "")
 	text = text.trim()
+	text = text.toLowerCase()
 	return text
 }
 
 let getRamdomRangeMillis = () => {
-	let max = 45000
-	let min = 22304
+	let max = 124533
+	let min = 54341
+	if(min < 10000){
+		console.err("Running with low delay, watch out for ip ban")
+	}
 	return Math.floor(Math.random() * (max - min) + min)
 }
 
